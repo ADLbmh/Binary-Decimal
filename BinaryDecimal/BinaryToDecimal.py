30d416c0f5f49de71e344496325034a67dd7fba3
ADLbmh <91545983+ADLbmh@users.noreply.github.com>
2021-09-28 09:58:58 -0400

    Add files via upload

---
+++ BinaryDecimal/BinaryToDecimal.py
@@ -0,0 +1,26 @@
+
+#================= by  ADlbmh =====================================
+
+userInput=(input("enter an upto 5 bit number: "))
+i,g,l= 0,0,0
+v = len(userInput)
+let=1
+for g in userInput:
+   z=let*int(g)
+   let*=2
+   l+=z
+while i < len(userInput):
+  if userInput[i]=='0' or userInput[i]=='1':
+      check=True
+      i+=1
+  else:
+      check=False
+      break
+if check==True   :
+    print("the value in decimal is: ",l)
+else:
+ print("not a valid binary number")
+
+
+
+#================================ END ==================================
