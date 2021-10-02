02557b525d433265bffee6158af616dc0c3798a0
ADLbmh <91545983+ADLbmh@users.noreply.github.com>
2021-10-01 21:42:13 -0400

    Update BinaryToDecimal.py

--- BinaryDecimal/BinaryToDecimal.py
+++ BinaryDecimal/BinaryToDecimal.py
@@ -5,10 +5,6 @@ userInput=(input("enter an upto 5 bit number: "))
 i,g,l= 0,0,0
 v = len(userInput)
 let=1
-for g in userInput:
-   z=let*int(g)
-   let*=2
-   l+=z
 while i < len(userInput):
   if userInput[i]=='0' or userInput[i]=='1':
       check=True
@@ -16,7 +12,8 @@ while i < len(userInput):
   else:
       check=False
       break
-if check==True   :
+if check==True:
+   l=int(userInput,2)
     print("the value in decimal is: ",l)
 else:
  print("not a valid binary number")
